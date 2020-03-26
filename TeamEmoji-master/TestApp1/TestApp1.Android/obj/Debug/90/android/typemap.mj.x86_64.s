0fbf6d87711efed21fa65d9a3b1ae7a5656369ef
Roy <fili0103@algonquinlive.com>
2020-03-26 16:58:42 -0400

    Added new background, Any problems can be talked about on the discord with fair feedback

---
+++ TeamEmoji-master/TestApp1/TestApp1.Android/obj/Debug/90/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data Hash: 77BC9DF3B8CBCF6B */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	446
+	/* entry-length */
+	.long	232
+	/* value-offset */
+	.long	130
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 103473
+	.include	"typemap.mj.inc"
